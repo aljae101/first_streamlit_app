9535f0739893eae301e33882e157f69f83a3fcfa
aljae101 <87036466+aljae101@users.noreply.github.com>
2022-10-04 11:21:17 +0200

    Fix: Small first letter jackfruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,5 +40,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 #New section, new text box, allow user to add fruit to list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
